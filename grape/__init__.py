4dbe9e9194ae36e83591e0d705e178bb3f9da8f7
jlinoff <joseph.linoff@esentire.com>
2021-01-07 11:07:38 -0800

    hygiene

--- grape/__init__.py
+++ grape/__init__.py
@@ -6,18 +6,21 @@ from pathlib import Path
 
 
 def load_version(fname: str) -> str:
-    '''
-    Load the module version from a __version__ file.
+    '''Load the module version from a __version__ file.
 
     This should follow semantic version 2.0 guidelines.
 
-    See: https://packaging.python.org/guides/single-sourcing-package-version/
+    For more information see
+    https://packaging.python.org/guides/single-sourcing-package-version/
 
     Args:
         fname - The version file name.
 
     Returns:
-        The contents.
+        contents: The version which is the content of the file.
+
+    Raises:
+        FileNotFoundError: If the __version__ file does not exist.
     '''
     with open(Path(Path(__file__).parent, fname)) as ifp:
         return ifp.read().strip()
