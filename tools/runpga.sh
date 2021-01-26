dfae2e0f68310ce0a0d57b064d1a264e3e30a2de
jlinoff <joseph.linoff@esentire.com>
2021-01-25 19:01:47 -0800

    lint using shellcheck

--- tools/runpga.sh
+++ tools/runpga.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2207
 #
 # Start a container that runs pgAdmin for the database you are
 # interested in. After it runs, it will provide information
@@ -12,19 +13,19 @@
 #    $ browser localhost:<port>  # <-- port is reported at the end of the run
 #
 set -e
-: ${PGAEMAIL:="zzz@login.com"}
-: ${PGAPASS:='password'}
+: "${PGAEMAIL:=zzz@login.com}"
+: "${PGAPASS:=password}"
 
 # Make sure that the user specified a container.
 PGNAME="$1"
 if [ -z "$PGNAME" ] ; then
     printf '\x1b[31m'
     printf 'ERROR:%d %s\n' $LINENO "container not specified"
-    CONTS=($(docker ps --filter name='.*pg' --format '{{ .Names }}' | sort -f))
-    echo "There are ${#CONTS[@]} candidate containers: ${CONTS[@]}"
+    CONTS=( $(docker ps --filter name='.*pg' --format '{{ .Names }}' | sort -f) )
+    echo "There are ${#CONTS[@]} candidate containers: ${CONTS[*]}"
     if (( ${#CONTS[@]} )) ; then
         echo "Any of these commands will work."
-        for CONT in ${CONTS[@]} ; do
+        for CONT in "${CONTS[@]}" ; do
             echo "   $0 ${CONT}"
         done
     else
@@ -35,7 +36,7 @@ if [ -z "$PGNAME" ] ; then
 fi
 
 # Make sure the container exists.
-CIDS=($(docker ps --filter "name=$PGNAME" --format '{{.ID}}'))
+CIDS=( $(docker ps --filter "name=$PGNAME" --format '{{.ID}}') )
 if (( ${#CIDS[@]} == 0 )) ; then
     printf '\x1b[31mERROR:%d %s\x1b[0m\n' $LINENO "container '$PGANAME' not found"
     exit 1
@@ -45,22 +46,22 @@ elif (( ${#CIDS[@]} > 1 )) ; then
 fi
 
 # Variables
-CID=${CIDS[0]}
-CPORT=$(docker inspect $CID | jq '.[] | .NetworkSettings.Ports."5432/tcp"[] | .HostPort' | tr -d '"')
+CID="${CIDS[0]}"
+CPORT=$(docker inspect "$CID" | jq '.[] | .NetworkSettings.Ports."5432/tcp"[] | .HostPort' | tr -d '"')
 PGANAME=$(echo -n "$PGNAME" | sed -e 's/..$/pa/')
 PGAPORT=$(( CPORT + 1 ))
 PGAPORT2=$(( PGAPORT + 1 ))
 
 # Start the container if it is not already running.
-CIDS=($(docker ps --filter "name=$PGANAME" --format '{{.ID}}'))
+CIDS=( $(docker ps --filter "name=$PGANAME" --format '{{.ID}}') )
 if (( ${#CIDS[@]} == 0 )) ; then
     # Make it idempotent.
     set -x
     docker run -d \
            -e PGADMIN_DEFAULT_EMAIL="$PGAEMAIL" \
            -e PGADMIN_DEFAULT_PASSWORD="$PGAPASS" \
-           -h $PGANAME \
-           --name $PGANAME \
+           -h "$PGANAME" \
+           --name "$PGANAME" \
            -p $PGAPORT:80 \
            -p $PGAPORT2:443 \
            dpage/pgadmin4
@@ -68,7 +69,7 @@ if (( ${#CIDS[@]} == 0 )) ; then
 fi
 
 # Summary
-PGAGW=$(docker inspect $PGANAME | jq '.[]|.NetworkSettings.Networks.bridge.Gateway' | tr -d '"')
+PGAGW=$(docker inspect "$PGANAME" | jq '.[]|.NetworkSettings.Networks.bridge.Gateway' | tr -d '"')
 cat <<EOF
 
 Summary
